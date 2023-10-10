ff3f9054a84b13dc3a69c3b1adbddb2308360e74
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-10-10 12:59:03 -0700

    Change JVM support to version 11 for max Android compat

--- amount/build.gradle.kts
+++ amount/build.gradle.kts
@@ -15,7 +15,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "17"
+                jvmTarget = "11"
             }
         }
     }
@@ -58,8 +58,8 @@ android {
         minSdk = 24
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
