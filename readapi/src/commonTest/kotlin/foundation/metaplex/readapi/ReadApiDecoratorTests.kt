226e2719d8a05f4ae75ab8844a66688f14a28e71
Marco Martinez <martinez.marco567@gmail.com>
2023-11-20 11:15:32 -0700

    extend timeout

--- readapi/src/commonTest/kotlin/foundation/metaplex/readapi/ReadApiDecoratorTests.kt
+++ readapi/src/commonTest/kotlin/foundation/metaplex/readapi/ReadApiDecoratorTests.kt
@@ -9,6 +9,7 @@ import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class ReadApiDecoratorTests {
+    private val testingTimeout = 30.seconds
     private var rpcUrl: String = "https://api.mainnet-beta.solana.com"
 
     private val readApiDecorator = ReadApiDecorator(
@@ -17,7 +18,7 @@ class ReadApiDecoratorTests {
     )
 
     @Test
-    fun testReadApiGetAssetsByOwner() = runTest {
+    fun testReadApiGetAssetsByOwner() = runTest(timeout = testingTimeout) {
         val randomPublicKey = PublicKey("2RtGg6fsFiiF1EQzHqbd66AhW7R5bWeQGpTbv2UMkCdW")
         val assets = readApiDecorator.getAssetsByOwner(GetAssetsByOwnerRpcInput(randomPublicKey))
         assertTrue { assets.total > 0 }
@@ -28,7 +29,7 @@ class ReadApiDecoratorTests {
     }
 
     @Test
-    fun testReadApiGetAsset() = runTest {
+    fun testReadApiGetAsset() = runTest(timeout = testingTimeout) {
         val randomAssetKey = PublicKey("BWvhiDKg1c1tB2nCSjmT6mxzxDr8RvTzzy8PSsYpFHY3")
         val asset = readApiDecorator.getAsset(randomAssetKey)
         assertEquals(randomAssetKey, asset.id)
@@ -37,14 +38,14 @@ class ReadApiDecoratorTests {
     }
 
     @Test
-    fun testReadApiGetAssetProof() = runTest {
+    fun testReadApiGetAssetProof() = runTest(timeout = testingTimeout) {
         val randomAssetKey = PublicKey("5Vaji1rsmhRCXJXPzZgXbfwbVvUtZGR2F9FXaaKQ1cME")
         val assetProof = readApiDecorator.getAssetProof(randomAssetKey)
         assertEquals(PublicKey("GZxgRaFoyxR2Vo3nUKVXQ2716Q7rdisjbNeu1m6SCoyH"), assetProof.proof.first())
     }
 
     @Test
-    fun testReadApiGetAssetsByGroup() = runTest {
+    fun testReadApiGetAssetsByGroup() = runTest(timeout = testingTimeout) {
         val assets = readApiDecorator.getAssetsByGroup(
             GetAssetsByGroupRpcInput(
                 "collection",
