fe1a479ef8088a90669017efc6a189662d340e51
funkatronics <funkatronicsmail@gmail.com>
2024-05-15 08:53:35 -0600

    remove unneeded workaround in network driver

--- rpc/src/commonMain/kotlin/foundation/metaplex/rpc/networking/NetworkDriver.kt
+++ rpc/src/commonMain/kotlin/foundation/metaplex/rpc/networking/NetworkDriver.kt
@@ -7,11 +7,6 @@ import io.ktor.client.request.request
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpMethod
-import kotlinx.serialization.builtins.serializer
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.jsonObject
 
 /**
  * Represents a network driver for making HTTP requests using an HttpClient.
@@ -33,9 +28,7 @@ class NetworkDriver(private val httpClient: HttpClient = NetworkClient()) : Http
                 headers.append(key, value)
             }
             method = HttpMethod(request.method)
-            // Workaround for RPC Response
-            // Removes a specific portion from the request body if present. RPCs complain about it
-            setBody(request.body?.replace("\"type\":\"com.funkatronics.rpccore.JsonRpc20Request\",", ""))
+            setBody(request.body)
         }
         println(response.bodyAsText())
         return response.bodyAsText()
